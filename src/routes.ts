2043171bd7cd5e43de1abc1d6998f8228b81aed0
joaoptgaino <joaoptgaino@hotmail.com>
2020-08-06 22:33:33 -0300

    Added transaction and error handling with try... catch

--- src/routes.ts
+++ src/routes.ts
@@ -19,33 +19,43 @@ routes.post('/classes', async (req, res) => {
         schedule
     } = req.body;
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
-    const user_id=insertedUsersIds[0];
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id
-    });
+    const trx = await db.transaction();
 
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem:ScheduleItem) =>{
-        return{
-            class_id,
-            week_day:scheduleItem.week_day,
-            from:convertHourToMinutes(scheduleItem.from),
-            to:convertHourToMinutes(scheduleItem.to)
-        };
-    });
-
-    await db('class_schedule').insert(classSchedule);
-
-    return res.send();
+    try{
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+        const user_id=insertedUsersIds[0];
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem:ScheduleItem) =>{
+            return{
+                class_id,
+                week_day:scheduleItem.week_day,
+                from:convertHourToMinutes(scheduleItem.from),
+                to:convertHourToMinutes(scheduleItem.to)
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+        await trx.commit();
+        return res.status(201).send();
+    }catch(err){
+        await trx.rollback();
+        return res.status(400).json({
+            error:'Unable to create new class'
+        })
+    }
+    
 });
 
 
