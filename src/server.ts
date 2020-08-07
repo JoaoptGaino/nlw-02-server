60d784ca0e7ce5b3f9c9e40770d230b3e5c938ab
joaoptgaino <joaoptgaino@hotmail.com>
2020-08-06 22:28:31 -0300

    added some new changes on inserting classes,user and schedule

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.json({message:'Hello world!!'});
-});
+
+app.use(routes);
 
 app.listen(3333);
